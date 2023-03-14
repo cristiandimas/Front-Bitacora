691ed4cd9bd92a98633638e6df2a6f261b7e862e
Cristian Barrantes <cristiandimas74@gmail.com>
2023-03-14 08:27:43 -0500

    Iniciando front de bitacora

--- src/components/FormReport.jsx
+++ src/components/FormReport.jsx
@@ -37,7 +37,7 @@ const FormReport = ({ reportLive, setReportLive, client, setCloseForm }) => {
         console.log(data);
 
       }).catch((err) => {
-        console.log(data);
+        console.log(err);
 
       });
   }
