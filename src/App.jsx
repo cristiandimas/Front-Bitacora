17e408808490cbda6dae20b7851de9c884e73fed
Cristian Barrantes <cristiandimas74@gmail.com>
2023-03-14 10:41:43 -0500

    Iniciando front de bitacora

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 
-import { Route, Routes } from 'react-router-dom'
+import { Link, Route, Routes } from 'react-router-dom'
 
 
 import './App.css'
 import ClientInfo from './components/ClientInfo'
 import Clients from './components/Clients'
-import FormReport from './components/FormReport'
+
 
 function App() {
 
@@ -13,13 +13,20 @@ function App() {
 
   return (
     <div className="App">
+      <header>
+        <nav>
+          <ul>
+            <li><Link to='/clientes'>Home</Link></li>
+          </ul>
+        </nav>
+      </header>
       <Routes>
         <Route path='/' element={<Clients />} />
         <Route path='/clientes' element={<Clients />} />
         <Route path='/clientes/:id' element={<ClientInfo />} />
       </Routes>
 
-      
+
 
     </div>
   )
